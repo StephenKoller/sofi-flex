18dceddff955eb59e66b95b951ee893ff6c15683
Chris DeMars <cdemars86@gmail.com>
2016-05-12 15:28:22 -0400

    fixed some things

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
-var prefix = require('gulp-autoprefixer');
+var gulp   = require('gulp');
+    sass   = require('gulp-sass');
+    cssmin = require('gulp-cssmin');
+    rename = require('gulp-rename');
+    prefix = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('scss/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(prefix('last 2 version'))
+    .pipe(prefix('last 2 versions'))
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function () {
